4baee9207c00cb78c7ea27535e1043c84c9bc0e4
Krehion <krehion@hotmail.com>
2024-03-19 14:30:51 +0100

    TODO : fermer la modale [Fix] #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,16 +8,33 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalBg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 
-// launch modal event
+// open modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+modalBg.addEventListener("click", (event) => {
+  if (event.target === modalBg) {
+    hideModal()
+  }
+})
+
+modalClose.addEventListener("click", (event) => {
+  if (event.target === modalClose) {
+    hideModal()
+  }
+})
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBg.style.display = "block";
 }
 
-
+// hide modal form
+function hideModal() {
+  modalBg.style.display = "none"
+}
